a8acdb43a8373f3a020ce548370fc6830ff85197
Igor Nikolaev <igor@spb.edu>
2017-09-20 09:48:16 +0300

    pmps.pl

--- pmps.pl
+++ pmps.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl -w
 use strict; use v5.20;
 use Pod::Usage; use Getopt::Long; use IO::Socket::Multicast; use IO::Interface::Simple;
-$/=1;
 
 my ($m_a, $m_p, $size, $mif, $srv, $V) = ('224.0.1.75', 5060, 10240, '', '', 0);
 my %pat = ( mac=>'sip:MAC(.*?)\@', lip=>'Via: SIP.*? ([\d\.]+):', ven=>'vend.r="(.*?)"',
@@ -13,16 +12,15 @@ GetOptions('help'=>sub{pod2usage(-input=>"$0.pod",-exitval=>0,-verbose=>2)},
 "verbose+"=>\$V, "url=s"=>\$url, "srver=s"=>\$srv, "interface=s"=>\$mif);
 $mif ||= (IO::Interface::Simple->interfaces)[1]; # get multicast interface if not defined
 $srv ||= (IO::Interface::Simple->new($mif))->address; # get '$srv' if not defined
-say "# $m_a:$m_p $mif:$srv $url" if $V;
+$/=1 and say "# $m_a:$m_p $mif:$srv $url" if $V;
 # listen new multicast socket $
 my $S=IO::Socket::Multicast->new(LocalPort=>$m_p, LocalAddr=>$m_a, ReuseAddr=>1) or die "$m_p $!";
 $S->mcast_add($m_a, $mif) or die "Couldn't multicast add $m_a $mif: $!\n";
 
 while(1) {
-	my $D; $S->recv($D,$size) or warn "# recv: $!"; $D =~ tr/\r//d;
-	print "# multicast ".$S->peerhost.':'.$S->peerport.' to '.$S->sockhost.':'.$S->sockport."\n" if $V;
-	next unless $D =~ /^SUBSCRIBE sip:/;
-	print $D if $V > 1;
+	my $D; $S->recv($D,$size) or warn "# recv: $!"; $D =~ tr/\r//d; # delete "\r" in SUBSCRIBE
+	say "# multicast ".$S->peerhost.':'.$S->peerport.' to '.$S->sockhost.':'.$S->sockport if $V;
+	next unless $D =~ /^SUBSCRIBE sip:/; print $D if $V > 1;
 
 	# phone params, ip from socket (may be nat'ed)
 	my %p = (ip=>$S->peerhost, mif=>$mif, srv=>$srv); # all trash to hash
@@ -34,7 +32,7 @@ while(1) {
 		or die "Could not create socket $p{ip}:$m_p $!";
 	my $OK = join "\r\n", ('SIP/2.0 200 OK',$p{via},"Contact: <sip:$p{ip}:$m_p>", $p{to}, $p{from},
 		$p{cid}, "CSeq: $cseq SUBSCRIBE", 'Expires: 0', 'Content-Length: 0', '');
-	$T->send($OK) or warn "Can't send OK to $p{ip} $!"; print "# OK:\n".$OK if $V > 1;
+	$T->send($OK) or warn "Can't send OK to $p{ip} $!"; print "# OK:\n$OK" if $V > 1;
 
 	$cseq++;	# Send NOTIFY packet
 	$url =~ s { \$\{?([\w\-]+)\}? } { $p{$1}||'' }gex;	# rewrite
@@ -43,5 +41,5 @@ while(1) {
 		'Subscription-State: terminated;reason=timeout',
 		'Event: ua-profile;profile-type="device";vendor="OEM";model="OEM";version="1"',
 		'Content-Length: '.length($url), '', $url);
-	$T->send($NFY) or warn "Can't send NOTIFY to $p{ip} $!"; print "# NFY:\n$NFY\n" if $V > 1;
+	$T->send($NFY) or warn "Can't send NOTIFY to $p{ip} $!"; say "# NFY:\n$NFY" if $V > 1;
 }

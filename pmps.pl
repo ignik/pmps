f2657fc214d84a4e83a6a6e2a040ee73490e5c08
Igor Nikolaev <igor@spb.edu>
2017-09-20 09:54:17 +0300

    pmps.pl

--- pmps.pl
+++ pmps.pl
@@ -1,23 +1,24 @@
 #!/usr/bin/perl -w
-use strict; use v5.20;
+use strict; use v5.20; # all perl scripts may be on one page sheet ;-)
 use Pod::Usage; use Getopt::Long; use IO::Socket::Multicast; use IO::Interface::Simple;
 
+my $URL = 'http://$srv/pmps/?hw=$mod&fw=$ver&mac=$mac&ip=$ip';
 my ($m_a, $m_p, $size, $mif, $srv, $V) = ('224.0.1.75', 5060, 10240, '', '', 0);
 my %pat = ( mac=>'sip:MAC(.*?)\@', lip=>'Via: SIP.*? ([\d\.]+):', ven=>'vend.r="(.*?)"',
 mod=>'model="(.*?)"', ver=>'version="(.*?)"', to=>'^(To:.*?)$', from=>'^(From:.*?)$',
 cid=>'^(Call-ID:.*?)$', via=>'^(Via:.*?)$', cseq=>'^CSeq: (\d+)' );
-my $url = 'http://$srv/pmps/?hw=$mod&fw=$ver&mac=$mac&ip=$ip';
 
 GetOptions('help'=>sub{pod2usage(-input=>"$0.pod",-exitval=>0,-verbose=>2)},
-"verbose+"=>\$V, "url=s"=>\$url, "srver=s"=>\$srv, "interface=s"=>\$mif);
+"verbose+"=>\$V, "url=s"=>\$URL, "srver=s"=>\$srv, "interface=s"=>\$mif);
 $mif ||= (IO::Interface::Simple->interfaces)[1]; # get multicast interface if not defined
 $srv ||= (IO::Interface::Simple->new($mif))->address; # get '$srv' if not defined
-$/=1 and say "# $m_a:$m_p $mif:$srv $url" if $V;
-# listen new multicast socket $
+$/=1 and say "# $m_a:$m_p $mif:$srv $URL" if $V;
+
+# listen new multicast $m_a:$m_p via $mif
 my $S=IO::Socket::Multicast->new(LocalPort=>$m_p, LocalAddr=>$m_a, ReuseAddr=>1) or die "$m_p $!";
 $S->mcast_add($m_a, $mif) or die "Couldn't multicast add $m_a $mif: $!\n";
 
-while(1) {
+while(1) { # wait SUBSCRIBE packet
 	my $D; $S->recv($D,$size) or warn "# recv: $!"; $D =~ tr/\r//d; # delete "\r" in SUBSCRIBE
 	say "# multicast ".$S->peerhost.':'.$S->peerport.' to '.$S->sockhost.':'.$S->sockport if $V;
 	next unless $D =~ /^SUBSCRIBE sip:/; print $D if $V > 1;
@@ -35,11 +36,11 @@ while(1) {
 	$T->send($OK) or warn "Can't send OK to $p{ip} $!"; print "# OK:\n$OK" if $V > 1;
 
 	$cseq++;	# Send NOTIFY packet
-	$url =~ s { \$\{?([\w\-]+)\}? } { $p{$1}||'' }gex;	# rewrite
+	$URL =~ s { \$\{?([\w\-]+)\}? } { $p{$1}||'' }gex;	# rewrite $URL by %p keys/values
 	my $NFY = join "\r\n", ("NOTIFY sip:$p{ip}:$m_p SIP/2.0", $p{via}, 'Max-Forwards: 20', $p{to},
 		$p{from}, $p{cid}, "CSeq: $cseq NOTIFY", 'Content-Type: application/url', 
 		'Subscription-State: terminated;reason=timeout',
 		'Event: ua-profile;profile-type="device";vendor="OEM";model="OEM";version="1"',
-		'Content-Length: '.length($url), '', $url);
+		'Content-Length: '.length($URL), '', $URL);
 	$T->send($NFY) or warn "Can't send NOTIFY to $p{ip} $!"; say "# NFY:\n$NFY" if $V > 1;
 }

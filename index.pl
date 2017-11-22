b0bf11635c57268dd2002ae1503a445e6a3060cf
Igor Nikolaev <igor@spb.edu>
2017-11-22 11:56:02 +0300

    * index.pl exec ./hardware/index.pl script

--- index.pl
+++ index.pl
@@ -1,14 +1,16 @@
 #!/usr/bin/perl -w
+#
+# https://github.com/ignik/pmps hardware specific configuration
+# cgi called 'hw=HardWare' and exec ./hardware/index.pl script
+#
 use strict;
-use File::Basename;
 use CGI qw(:all);
-undef $ENV{PATH};
+use File::Basename;
 chdir dirname $0;
 
+undef $ENV{PATH};
+
 sub bug ($) { print header, start_html, $_[0], end_html; exit; }
-my $hw = lc (param('hw') || ''); # onl
-bug "incorrect hw $hw" unless $hw =~ /(\w[\w\-]*)/;
-my $cmd = "./$1.pl";
-exit if $cmd eq 'index.pl';
-exec ($cmd, @ARGV) if -x $cmd;
-bug "No provisioning for $hw";
+my $hw = lc ( param('hw') || '' );
+bug "incorrect hw $hw" unless $hw =~ /(\w[\w\-]*)/;	# must have letters
+( my $cmd = "$1/index.pl" ) ? exec ($cmd, @ARGV) : bug "No file $cmd";
